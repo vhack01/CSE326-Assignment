e76d07aeb29af62c3d52a796d523a687383d731c
Vishwas kumar <34247798+vhack01@users.noreply.github.com>
2022-05-02 23:39:59 +0530

    Delete Blog directory

--- Blog/javascript/login_js.js
+++
@@ -1,8 +0,0 @@
-function validateLogin() {
-    var eml = document.getElementById("inp_l1").value;
-    var pass = document.getElementById("inp_l2").value;
-
-    if(eml.length == 0 || pass.length == 0) {
-        alert("Enter Email Id and Password.");
-    }
-}
